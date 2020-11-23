3d196c976b5c91f693192d44e278754ccc727e0f
Yassir BENDOU <yassir.bendou@gmail.com>
2020-11-23 18:54:07 +0100

    Small modification on cleaning function

--- explore/utils.py
+++ explore/utils.py
@@ -8,12 +8,17 @@ def clean_noisy_data(dataset,classes = 2):
     if 'id' in dataset.columns :
         dataset = dataset.drop(columns = ['id']) # Drop id column as it's not relevent for predictions
 
-    #Changing numerical data into float types except for the output variable
+    #Changing numerical data into float types 
+    output_column = dataset.columns[-1]
+
     string_columns = []
+    output_is_string = False
     for c in dataset.columns : 
         try : 
             dataset[c] = dataset[c].astype(float)
         except ValueError :
+            if c == output_column:
+                output_is_string = True
             string_columns.append(c)
             
     for c in string_columns :
@@ -21,9 +26,9 @@ def clean_noisy_data(dataset,classes = 2):
         dataset[c] = dataset[c].replace('?',np.nan)
     
     #Ordinal encoding of the output variable 
-    output_column = dataset.columns[-1]
-    outputs = dataset[output_column].unique()
-    assert len(outputs) == classes, 'Error the number of output classes should be the same as the one in the dataset'
-    dataset[output_column] = 1*(dataset[output_column] == outputs[0]) #return an ordinal encoding of the output variable
-    
+    if output_is_string:
+        outputs = dataset[output_column].unique()
+        assert len(outputs) == classes, 'Error the number of output classes should be the same as the one in the dataset'
+        dataset[output_column] = 1*(dataset[output_column] == outputs[0]) #return an ordinal encoding of the output variable
+    dataset[output_column] = dataset[output_column].astype(int)
     return dataset
