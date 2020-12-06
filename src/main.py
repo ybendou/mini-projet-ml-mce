f159821a8d9bf0818e4ade5bba11684e1b59969f
hafsa17 <hafsa.ouled-attou@imt-atlantique.net>
2020-12-06 19:27:28 +0100

    Fixing PCA feature selection and Adding sphinx documentation format

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
 import json 
 
+#This is a particular class to include dictionaries in the parsers
 class StoreDictKeyPair(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
         self._nargs = nargs
@@ -22,32 +23,33 @@ class StoreDictKeyPair(argparse.Action):
         setattr(namespace, self.dest, my_dict)
 
 
-# All the args argument:
-parser = argparse.ArgumentParser(description='Main-Code')
-parser.add_argument('--model', '-m', type=str, default='SVC',help = "model to run on the dataset, options are \n{'SVC', 'LogisticRegression', 'SGDClassifier', 'RandomForestClassifier', 'AdaBoostClassifier'}")
-parser.add_argument('--dataset','-d', type=str, default='kidney', help="dataset type, options are {'kidney','banknote'}, default is 'kidney'")
-parser.add_argument("--parameters",'-p' , dest="my_dict", action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL",help="model paramaters, dictionnary of the parameters and their values, example for SVC : -p kernel=rbf C=10 degree=2 gamma=auto")
-parser.add_argument('--pca', type=bool, default=True, help="PCA type, if True keeps the original features, if False use the PCA new features, default is True")
-parser.add_argument('-v', '--cut_off_variance',type=float, default=0.95, help="PCA cut-off variance default is 0.95")
+# All the args arguments:
+parser = argparse.ArgumentParser(description='Main-Code') 
+parser.add_argument('--model', '-m', type=str, default='SVC',help = "model to run on the dataset, options are \n{'SVC', 'LogisticRegression', 'SGDClassifier', 'RandomForestClassifier', 'AdaBoostClassifier'}") # model choice
+parser.add_argument('--dataset','-d', type=str, default='kidney', help="dataset type, options are {'kidney','banknote'}, default is 'kidney'") # dataset choice
+parser.add_argument("--parameters",'-p' , dest="my_dict", action=StoreDictKeyPair, nargs="+", metavar="KEY=VAL",help="model paramaters, dictionary of the parameters and their values, example for SVC : -p kernel=rbf C=10 degree=2 gamma=auto") # parameters choice, it's a dictionary
+parser.add_argument('--pca', type=bool, default=True, help="Apply PCA, if True applies PCA algorithm") #Choice to keep the original features or to use the PCA coefficients instead
+parser.add_argument('-v', '--cut_off_variance',type=float, default=0.95, help="PCA cut-off variance or the threshold ratio of explained variance. Default is 0.95") #Ratio threshold of variance explained
 
 
 data_paths = {'kidney':'../data/kidney_disease/kidney_disease.csv',
-             'banknote':'../data/banknote/data_banknote_authentication.txt'}
+             'banknote':'../data/banknote/data_banknote_authentication.txt'} # Dictionary of datasets paths
 
-models_dict = {'SVC':{'model':SVC,
+# Dictionary of models and their corresponding parameters for the gridsearch
+models_dict = {'SVC':{'model':SVC, #Support vector Classifier
                       'parameters':{'kernel':['linear', 'rbf', 'sigmoid', 'poly'], 
                                                 'C'     :[1, 10], 
                                                 'degree': [2, 3],
                                                 'gamma' : ['scale', 'auto']
                                    }
                      },
-               'LogisticRegression':{'model':LogisticRegression,
+               'LogisticRegression':{'model':LogisticRegression, # Logistic Regression
                                     'parameters':{'C':[1, 10], 
                                                 'fit_intercept' : [True,False],
                                                 'intercept_scaling' : [1,10],
                                                  }
                                     },
-                'SGDClassifier':{'model':SGDClassifier,
+                'SGDClassifier':{'model':SGDClassifier, # Stochastic gradient descent classifier
                                     'parameters':{'loss':['hinge', 'log', 'modified_huber', 'squared_hinge', 'perceptron'], 
                                                 'penalty':['l1', 'l2'], 
                                                 'l1_ratiofloat' : [0.15,0.5,0.9],
@@ -76,26 +78,28 @@ models_dict = {'SVC':{'model':SVC,
                 }
 
 if __name__ == '__main__':
-    args = parser.parse_args()    
-    data_str  = args.dataset
-    model_str = args.model
-    params    = args.my_dict
+    args = parser.parse_args()  #Initiate the parser
+    data_str  = args.dataset    # dataset choice
+    model_str = args.model      # model choice
+    params    = args.my_dict    # get the parameters dictionary provided by the user
     print('params',params)
     print('Extracting data')
-    data = pd.read_csv(data_paths[data_str])  
+    data = pd.read_csv(data_paths[data_str]) # Fetch the data
     print('Cleaning data')    
-    data = clean_noisy_data(data,classes = 2)
-    num_variables, categ_variables = detect_type(data.iloc[:,:-1])
-    data.iloc[:,:-1] = replace_missing(data.iloc[:,:-1], num_variables, categ_variables)
-    data.iloc[:,:-1] = center_encode(data, num_variables, categ_variables)
-    dataset = feature_selection(data,cut_off_variance=args.cut_off_variance,keep_features=args.pca)
+    data = clean_noisy_data(data,classes = 2)  # Clean the dataset
+    num_variables, categ_variables = detect_type(data.iloc[:,:-1]) # Detect numerical and categorical data
+    data.iloc[:,:-1] = replace_missing(data.iloc[:,:-1], num_variables, categ_variables) # Replace missing values
+    data.iloc[:,:-1] = center_encode(data, num_variables, categ_variables)  # scale values and ordinal ordinalencode them
     
-    X,y = dataset.values[:,:-1],dataset.values[:,-1]
+    if args.pca: 
+        dataset = feature_selection(data,cut_off_variance=args.cut_off_variance)  # Compress the data using PCA
+    
+    X,y = dataset.values[:,:-1],dataset.values[:,-1] 
     X_train, X_test, y_train, y_test = split_data(X,y)
     
     model_choice = models_dict[model_str]
     model = model_choice['model']
-    if params == None : 
+    if params == None : # If the user doesn't provide any parameters for the model, we run the gridsearch and pick the best ones
         print(f'Chosen model and its different possible parameters: {model_choice}')
         print(f'running grid search to find the best parameters for the {data_str} dataset')
         parameters = model_choice['parameters']
@@ -103,9 +107,9 @@ if __name__ == '__main__':
         print(f'Best parameters for {model_str} are {params}. \nBest Score is {best_score}')
 
     print(f'Training with parameters : {params}')    
-    score_val,clf = training(model, params, X_train, y_train)
+    score_val,clf = training(model, params, X_train, y_train) # Train the model 
     print(f'Cross Validation Score : {score_val}')
-    score_test = test_evaluate(clf,X_test,y_test)
+    score_test = test_evaluate(clf,X_test,y_test) # Run the model on test data
     print(f'F1 score on test data : {score_test}')
 
     
--- src/utils.py
+++ src/utils.py
@@ -13,9 +13,13 @@ def clean_noisy_data(dataset,classes = 2):
     Clean the data with replacing the miss-filled values by the correct ones and defining the right types for the dataset
     variables.
     
-    Input : The dataset to clean
-    Output : Cleaned dataset
-    
+    Args: 
+        :param dataset: The dataset to clean.
+        :type  dataset: pandas.DataFrame
+            
+    Returns: 
+        :param dataset: Cleaned dataset.
+        :type dataset: pandas.DataFrame
     """
     
     if 'id' in dataset.columns :
@@ -23,7 +27,7 @@ def clean_noisy_data(dataset,classes = 2):
     
     
     for c in dataset.columns :
-        if dataset[c].dtype != 'float32' and dataset[c].dtype != 'float64' and dataset[c].dtype != 'int32'  and dataset[c].dtype != 'int64' :
+        if dataset[c].dtype not in ['float32','float64','float','int32','int64','int']:
             dataset[c] = dataset[c].str.replace('\t','')
             dataset[c] = dataset[c].replace('?',np.nan)
             dataset[c] = dataset[c].str.replace(' ','')
@@ -44,7 +48,6 @@ def clean_noisy_data(dataset,classes = 2):
                 output_is_string = True
             string_columns.append(c)
             
- 
     #Ordinal encoding of the output variable 
     if output_is_string:
         outputs = dataset[output_column].unique()
@@ -59,9 +62,17 @@ def clean_noisy_data(dataset,classes = 2):
 def detect_type(data):
     """
     Detecting the type of the variables numerical or categorical
-    
-    Input : Dataset 
-    Output : Lists of numerical variables and categorial ones
+
+    Args: 
+        :param data: The dataset.
+        :type  data: pandas.DataFrame
+            
+    Returns: 
+        :param num_variables: List of columns with numerical values.
+        :type num_variables: list
+        
+        :param categ_variables: List of columns with categorical values.
+        :type categ_variables: list
     """
     
     num_variables = []
@@ -69,7 +80,7 @@ def detect_type(data):
     columns = list(data.columns)
     n = len(columns)
     for i in range(n):
-        if data[columns[i]].dtype == 'int' or data[columns[i]].dtype == 'float': #Checking if the variable is numerical
+        if data[columns[i]].dtype in  ['int32','int64','float32','float64','int','float']: #Checking if the variable is numerical
             num_variables.append(columns[i])
         else : 
             categ_variables.append(columns[i])
@@ -80,9 +91,26 @@ def replace_missing(data, num_variables, categ_variables, num_strategy = 'mean',
     """
     Replacing the missing values in categorical variables and numerical variables by 2 corresponding strategies
     (mean for numerical variables and the most frequent value for categorical varibles for example)
-    
-    Input : Dataset, numerical variables of the data, categorial variables of the data and the defined strategies
-    Output : A transformed dataset with missing values filled
+
+    Args: 
+        :param data: The dataset.
+        :type  data: pandas.DataFrame
+        
+        :param num_variables: List of columns with numerical values.
+        :type num_variables: list
+        
+        :param categ_variables: List of columns with categorical values.
+        :type categ_variables: list
+        
+        :param num_strategy: The defined strategy to replace the numerical missing values, default is mean.
+        :type num_strategy: str
+        
+        :param categ_strategy: The defined strategy to replace the categorical missing values, default is most_frequent.
+        :type categ_strategy: str
+               
+    Returns: 
+        :param data_tr_table: A transformed dataset with missing values filled.
+        :type data_tr_table: pandas.DataFrame
     """
     
     ct = ColumnTransformer([("categ_imput", SimpleImputer(missing_values = np.nan, strategy = categ_strategy), categ_variables),
@@ -95,19 +123,29 @@ def replace_missing(data, num_variables, categ_variables, num_strategy = 'mean',
 def center_encode(data, num_variables, categ_variables):
     """
     Centring and normalizing the data. Transforming the categorical variables.
-    
-    Input : Dataset, numerical variables of the data and categorical variables of the data.
-    Output : Transformed dataset
-    
+   
+    Args: 
+        :param data: The dataset.
+        :type  data: pandas.DataFrame
+        
+        :param num_variables: List of columns with numerical values.
+        :type num_variables: list
+        
+        :param categ_variables: List of columns with categorical values.
+        :type categ_variables: list
+            
+    Returns: 
+        :param data_tr_table: A centered and encoded dataset.
+        :type data_tr_table: pandas.DataFrame
     """
     cat_enc = OrdinalEncoder()
     center_norm = StandardScaler()
-    if categ_variables != [] and categ_variables != []:
+    if categ_variables != [] and num_variables != []:
         categ_data = data[categ_variables]
         num_data = data[num_variables]
-        data_transformed_cat = cat_enc.fit_transform(categ_data) #.toarray() #ordinal encode catageorical data 
+        data_transformed_cat = cat_enc.fit_transform(categ_data) 
         data_transformed_num = center_norm.fit_transform(num_data) # center and normalize numerical data
-        categ_columns = categ_variables #cat_enc.get_feature_names(categ_variables)
+        categ_columns = categ_variables 
         columns = list(categ_columns) + num_variables
         data_transformed = np.concatenate((data_transformed_cat, data_transformed_num), axis = 1)
         data_tr_table = pd.DataFrame(data_transformed, columns = columns)
@@ -124,52 +162,62 @@ def center_encode(data, num_variables, categ_variables):
     return data_tr_table
 
 
-def feature_selection(dataset,cut_off_variance=0.95,keep_features=True):
+def feature_selection(dataset,cut_off_variance=0.95):
     """
-        Applies Feature selection using PCA. We only keep the features which have the highest absolute coefficient to the principal component vectors.
-        We also fix the number of the remaining vectors based on the number of components which garantees 95% of the original variance of the dataset.
+    Applies Feature selection using PCA. We fix the number of the remaining vectors based on the number of components which garantees 95% of the original variance of the dataset.
+        
+    Args: 
+        :param dataset: The dataset.
+        :type  dataset: pandas.DataFrame
+        
+        :param cut_off_variance: The threshold ratio of the explained variance, takes values between 0 and 1, default is 0.95.
+        :type cut_off_variance: float
+            
+    Returns: 
+        :param data_compressed: The compressed dataset.
+        :type data_compressed: pandas.DataFrame
     """
     X,_ = dataset.values[:,:-1],dataset.values[:,-1]
-    pca = PCA()
+    pca = PCA(n_components=len(dataset.columns)-1)
     pca.fit(X)
-    y = np.cumsum(pca.explained_variance_ratio_)
+    y = np.cumsum(pca.explained_variance_ratio_) # get the cumulative explained variance
 
-    
     number_of_features = len(dataset.columns) - sum(y>=cut_off_variance)
     print(f'number of features selected : {number_of_features}')
     
-    if keep_features : 
-        pca = PCA(n_components=2)
-        pca.fit(X)
-        features_importance_sorted = np.argsort(pca.components_[0])
-        features_remaining = features_importance_sorted[:number_of_features]
-        data_compressed = dataset.iloc[:,list(features_remaining)+[len(dataset.columns)-1]]
-    else :
-        pca = PCA(n_components=number_of_features)
-        new_X = pca.fit_transform(X)        
-        data_compressed = pd.DataFrame(new_X)
-        data_compressed[dataset.columns[-1]] = dataset.iloc[:,-1]
-        
+    new_X = pca.fit_transform(X) # get the projections on the eigen vectors of PCA.     
+    data_compressed = pd.DataFrame(new_X[:,:number_of_features]) # only keep the desired features according to the cut_off_variance
+    data_compressed[dataset.columns[-1]] = dataset.iloc[:,-1]
+
     return data_compressed
 
 def split_data(X, y):
     """
     Split the data into training set and validation set
     
-    Input : Data and output
-    Output : Training set and validation set of the data, training set and validation set of the class
-    
+    Args: 
+        :param X: The features
+        :type  X: numpy.array
+
+        :param y: labels
+        :type  y: numpy.array
+
+    Returns: Training set and validation set of the data, training set and validation set of the class
     """
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
     return X_train, X_test, y_train, y_test
 
-def determine_combinaisons(parameters):
+def determine_combinations(parameters):
     """
     Determine all possible combinaisons to have from a defined set of parameters of a model
     
-    Input : Dictionary of parameters
-    Output : List of all combinaisons, each combinaison of parameters defined as a dictionary 
-    
+    Args: 
+        :param parameters:  Dictionary of parameters.
+        :type  parameters: dict
+
+    Returns: 
+        :param comb_parameters: List of all combinaisons, each combinaison of parameters defined as a dictionary.
+        :type comb_parameters: list
     """
     parameters_values = list(parameters.values())
     combinations = list(itertools.product(*parameters_values))
@@ -189,6 +237,24 @@ def training(model, parameters, X, y):
     Input : Model, parameters of the model, data, class
     Output : the score of the cross validation
     
+    Args: 
+        :param model:  the defined model.
+        :type  model: object
+        
+        :param parameters: parameters of the model.
+        :type  parameters: dict
+
+        :param X: features.
+        :type  X: numpy.array
+        
+        :param y: labels.
+        :type  y: numpy.array
+    Returns: 
+        :param score: the score of the cross validation.
+        :type score: float
+         
+        :param clf: the trained model.
+        :type clf: object
     """
     clf = model(**parameters)
     scores = cross_val_score(clf, X, y, cv=5, scoring='f1_macro')
@@ -201,17 +267,43 @@ def select_params(model, parameters, X, y):
     Selecting the best parameters to take for the model 
     
     Input : Model, dictionary of possible parameters, Data, class
-    Output : Chosen combinaison of parameters, score of the cross validation with this combinaison
+    Output : 
     
+    Args: 
+        :param model:  the defined model.
+        :type  model: object
+        
+        :param parameters: all possible parameters of the model for the gridsearch, format is a dictionary of lists. \nExample : 'parameters':{'loss':['log', 'squared_hinge', 'perceptron']}} 
+        :type  parameters: dict
+
+        :param X: features.
+        :type  X: numpy.array
+        
+        :param y: labels.
+        :type  y: numpy.array
+    Returns: 
+        :param chosen_params: Chosen combinaison of parameters, score of the cross validation with this combinaison.
+        :type chosen_params: dict
+        
+        :param score_max: score of the chosen parameters, which is the highest score.
+        :type score_max: float
     """
-    comb_parameters = determine_combinaisons(parameters)
+    
+    comb_parameters = determine_combinations(parameters) # Get all the possible combinations
     total_scores = []
-    for i in range(len(comb_parameters)):
+    for i in range(len(comb_parameters)): 
         total_scores.append(training(model, comb_parameters[i], X, y)[0])
     score_max = np.max(total_scores)
     ind_max = np.argmax(total_scores)
-    return comb_parameters[ind_max], score_max
+    chosen_params = comb_parameters[ind_max] # return the parameters with the highest score.
+    
+    return chosen_params, score_max
+
+
 def test_evaluate(model,X,y):
+    """
+    Evaluate the model on test data and returns the f1 score.
+    """
     y_pred = model.predict(X)
     score = f1_score(y, y_pred,average='macro')
     return score
